e919c80c114219669b90cb89aa88253739413a80
aditya-bhujle <aditya.n.bhujle@gmail.com>
2020-07-16 14:17:56 -0400

    Cleanup post modal sidebar

--- src/components/pages/post/Sidebar.jsx
+++ src/components/pages/post/Sidebar.jsx
@@ -19,53 +19,6 @@ export default function PostSidebar({ info, close }) {
 			) : (
 				<CardPreviewInfo loading subtitle />
 			)}
-			<div className="hub_card bot_padding">
-				<h3>Read More</h3>
-				<div className="flex">
-					<button
-						className="button select no_margin inside"
-						style={{
-							flexGrow: 1,
-							flexShrink: 1,
-							flexBasis: "0%",
-							borderStyle: "solid",
-						}}
-					>
-						Previous Post
-					</button>
-					<button
-						className="button"
-						style={{ flexGrow: 1, flexShrink: 1, flexBasis: "0%" }}
-					>
-						Next Post
-					</button>
-				</div>
-			</div>
-			<CardPreviewList
-				title="Hot Posts"
-				elements={[
-					{
-						header: "Taking this class with Logic and Algorithms?",
-						right: "Yesterday",
-						content: [
-							<strong className="list_category red" key="red">
-								Not In Class
-							</strong>,
-							" ⋅ Anonymous ⋅ 4 replies",
-						],
-					},
-					{
-						header: "Who else has Assignment 3 pushed back",
-						right: "Yesterday",
-						content: [
-							<strong className="list_category" key="blue">
-								Hub
-							</strong>,
-							" ⋅ Anonymous ⋅ 4 replies",
-						],
-					},
-				]}
-			/>
 		</div>
 	);
 }
