7be8060df0b5c61a93eaf5a9f89ef6e5615938d9
aditya-bhujle <aditya.n.bhujle@gmail.com>
2020-07-16 10:45:21 -0400

    Joining hub/teacher doesn't show joined for other

--- src/components/header/ClassHeader.jsx
+++ src/components/header/ClassHeader.jsx
@@ -17,13 +17,28 @@ export default function ClassHeader({
 	const { addToast } = useToasts();
 	const [joined, setJoined] = useState(false);
 
+	const userRef = db.collection("users").doc(userInfo.id);
+
 	useEffect(() => {
 		if (userInfo) {
 			userInfo.classes.forEach((userClass) => {
 				if (userClass.id === schoolClass.id) setJoined(true);
 			});
 		}
-	}, [schoolClass.id, userInfo]);
+		if (userInfo) {
+			let foundMatch = false;
+			userInfo.classes.forEach((userClass) => {
+				if (userClass.id === schoolClass.id) {
+					if (userClass.teacher && teacherId === userClass.teacher.id) {
+						foundMatch = true;
+					} else if (!userClass.teacher && !teacherId) {
+						foundMatch = true;
+					}
+				}
+			});
+			setJoined(foundMatch);
+		}
+	}, [schoolClass.id, teacherId, userInfo]);
 
 	if (loading)
 		return (
@@ -35,22 +50,55 @@ export default function ClassHeader({
 			</Header>
 		);
 
+	async function removeClassFromUser() {
+		const removeClasses = userInfo.classes.filter(
+			(userClass) => userClass.id === schoolClass.id
+		);
+
+		for (let i = 0; i < removeClasses.length; i++) {
+			await userRef.update({
+				classes: firestore.FieldValue.arrayRemove(removeClasses[i]),
+			});
+		}
+	}
+
+	// Removes member from other Hub (if teacher removes from hub vice versa) and
+	// if its removing a teacher then will subtract member from teachers.members in hub
+	async function removeClassConflict() {
+		let otherTeacherId;
+
+		const similarClasses = userInfo.classes.filter(
+			(userClass) => userClass.id === schoolClass.id
+		);
+
+		for (let i = 0; i < similarClasses.length; i++) {
+			const similarClass = similarClasses[i];
+
+			if (similarClass.teacher && similarClass.teacher.id)
+				otherTeacherId = similarClass.teacher.id;
+		}
+
+		// If I'm joined to a teacher somewhere else, subtract the teacher member, else subtract the hub member
+		let otherRef = otherTeacherId
+			? classRef.collection("teachers").doc(otherTeacherId)
+			: classRef.parent.parent;
+		await otherRef.update({ members: firestore.FieldValue.increment(-1) });
+
+		if (otherTeacherId) {
+			const updateObject = {};
+			updateObject[
+				`teachers.${otherTeacherId}.members`
+			] = firestore.FieldValue.increment(-1);
+			await classRef.update(updateObject);
+		}
+	}
+
 	async function toggleJoin() {
 		setJoined(!joined);
 
-		const removeClass = userInfo.classes.filter(
-			(userClass) => userClass.id === schoolClass.id
-		);
-		const userRef = db.collection("users").doc(userInfo.id);
-
 		try {
 			if (joined) {
-				for (let i = 0; i < removeClass.length; i++)
-					await userRef.update({
-						classes: firestore.FieldValue.arrayRemove(removeClass[i]),
-					});
-
-				await classRef.update({ members: firestore.FieldValue.increment(-1) });
+				removeClassFromUser();
 
 				if (teacherId) {
 					const updateObject = {};
@@ -60,6 +108,18 @@ export default function ClassHeader({
 					await classRef.parent.parent.update(updateObject);
 				}
 			} else {
+				const isInClass = //Checks if theres a conflict
+					userInfo.classes.filter(
+						(userClass) => userClass.id === schoolClass.id
+					).length > 0;
+
+				// User is not inside this hub
+				if (isInClass) removeClassConflict();
+
+				removeClassFromUser();
+
+				// Add to class
+
 				await userRef.update({
 					classes: firestore.FieldValue.arrayUnion(schoolClass),
 				});
